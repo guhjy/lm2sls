e6a12f0e0229f37847e0c433a4d9e7bebd1157dd
John Fox <jfox@mcmaster.ca>
2019-07-25 14:19:01 -0400

    tweak tests

--- tests/testthat/test-case-deletion.R
+++ tests/testthat/test-case-deletion.R
@@ -1,6 +1,6 @@
 m <- lm2sls(Q ~ P + D, instruments = ~ D + F + A, data=Kmenta)
 m1 <- update(m, subset = -1)
-test_that("dfbeta computed correctly", {
+test_that("dfbeta computed correctly 1", {
   expect_equal(dfbeta(m)[1, ], coef(m) - coef(m1))
 })
 
@@ -11,12 +11,12 @@ test_that("hatvalues computed correctly", {
   expect_equal(as.vector(hatvalues(m.2sls)), as.vector(hatvalues(m.lm)))
 })
 
-test_that("cookd computed correctly", {
+test_that("Cook's distances computed correctly", {
   expect_equal(cooks.distance(m.2sls), cooks.distance(m.lm))
 })
 
-test_that("dffits computed correcxtly", {
-  expect_equal(as.vector(dffits(m.2sls)), as.vector(dffits(m.lm)))
+test_that("dfbeta computed correctly 2", {
+  expect_equal(dfbeta(m.2sls), dfbeta(m.lm))
 })
 
 test_that("rstudent computed correctly", {
